76daf324fc0893d16b82321acea251c006d8bf67
Tim King <tim@angrymango.com>
2019-05-21 10:22:33 +1200

    Updated to split sms code into 4 boxes

--- login/resources/js/register.js
+++ login/resources/js/register.js
@@ -1,3 +1,32 @@
 $(function() {
     
+    setCharAt = (str,index,chr) => {
+        if(index > str.length-1) return str;
+        return str.substr(0,index) + chr + str.substr(index+1);
+    }
+
+    onEnterSmsCode = (input, idx, focusNextName, event, hiddenFieldName) => {
+        console.log("onChangeSmsCode", idx, event);
+
+        // remove anything except numbers and only allow a single character
+        input.value = input.value.replace(/[^\d]/g, '');
+        if (input.value.length) {
+            input.value = input.value[input.value.length - 1];
+        }
+
+        if (input.value.length <= 0) {
+            return;
+        }
+
+        // update hidden field
+        var hiddenField = document.getElementsByName(hiddenFieldName)[0];
+        hiddenField.value = setCharAt(hiddenField.value, idx, input.value);
+        
+        var elements = document.getElementsByName(focusNextName);
+        if (elements.length) {
+            var focusNextElement = elements[0];
+            focusNextElement.focus();
+            focusNextElement.select();
+        }
+    }
 });
